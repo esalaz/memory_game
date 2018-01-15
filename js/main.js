88917cd29450600ac6cc8adc7ababea65cbb922a
Erik Salazar <eriksalazar@eriks-mbp.attlocal.net>
2018-01-14 23:25:38 -0800

    Changed array to an Object and added key pairs of, rank, suit, and cardImage

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
-
-console.log('Up and running!');
-
-var cards = ['Queen', 'Queen', 'King', 'King'];
+var cards = [
+{
+  rank: 'Queen',
+  suit: 'Hearts',
+  cardImage: 'images/queen-of-hearts.png',
+},
+{
+  rank: 'Queen',
+  suit: 'Diamonds',
+  cardImage: 'images/queen-of-hearts.png',
+},
+{
+  rank: 'King',
+  suit: 'Hearts',
+  cardImage: 'images/queen-of-hearts.png',
+},
+{
+  rank: 'King',
+  suit: 'Diamonds',
+  cardImage: 'images/queen-of-hearts.png',
+},
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -16,8 +34,10 @@ function checkForMatch() {
 }//checkForMatch function
 
 function flipCard(cardId) {
-  console.log('User flipped ' + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log('User flipped ' + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
   checkForMatch();
 }//flip card function ends
 
